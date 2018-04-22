3d23c7cd9bdf190803f84f74b1497b76a858c63f
Watermelon <watermelon8157@gmail.com>
2018-04-23 01:28:49 +0800

    update

--- app.py
+++ app.py
@@ -38,13 +38,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	msg = event.message.text
-    msg = msg.encode('utf-8')
-    message = TextSendMessage(text=msg)
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         message)
 
 import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
