6853451f816451af806580185bdf1ae9ce076774
Watermelon <watermelon8157@gmail.com>
2018-04-23 01:23:41 +0800

    app.py update

--- app.py
+++ app.py
@@ -38,7 +38,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+	msg = event.message.text
+    print(msg)
+    msg = msg.encode('utf-8')
+    message = TextSendMessage(text=msg)
     line_bot_api.reply_message(
         event.reply_token,
         message)
